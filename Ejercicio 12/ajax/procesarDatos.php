6a8cc5607fb79111363e5194936836d23e08e90a
gfemma <81449384+gfemma@users.noreply.github.com>
2021-06-18 16:57:42 -0300

    Tomo los datos y realizo validaciones

--- Ejercicio 12/ajax/procesarDatos.php
+++ Ejercicio 12/ajax/procesarDatos.php
@@ -1,3 +1,26 @@
 <?php
-    print_r($_REQUEST);
+    $data = $_REQUEST;
+    
+    $reg = array();
+
+    $reg['nombre'] = (isset($data['nombre']))? $data['nombre']:null;
+    $reg['apellido'] = (isset($data['apellido']))? $data['apellido']:null;
+    $reg['edad'] = (isset($data['edad']))? $data['edad']:null;
+    $reg['genero'] = (isset($data['genero']))? $data['genero']:null;
+    $reg['email'] = (isset($data['email']))? $data['email']:null;
+    $reg['telefono'] = (isset($data['telefono']))? $data['telefono']:null;
+    $reg['celular'] = (isset($data['celular']))? $data['celular']:null;
+    $reg['imagen'] = (isset($data['imagen']))? $data['imagen']:null;
+    $reg['pais'] = (isset($data['pais']))? $data['pais']:null;
+    $reg['estado'] = (isset($data['estado']))? $data['estado']:null;
+    $reg['latitud'] = (isset($data['latitud']))? $data['latitud']:null;
+    $reg['longitud'] = (isset($data['longitud']))? $data['longitud']:null;
+
+    foreach($reg as $key => $value){
+        if(empty($value)){
+            $reg[$key] = "No encontrado";
+        }
+    }
+//Ahora realizo la vista que ira dentro de la card
 ?>
+
